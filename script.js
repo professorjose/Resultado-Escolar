e67549abe58900219ed25f8d8644c1178c5d0dc4
José Alves de Oliveira <160446328+professorjose@users.noreply.github.com>
2024-10-29 16:37:35 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+function verificar(){
+    var n1 = document.getElementById('nota1').value;
+    var n1 = document.getElementById('nota1').value;
+    var n1 = document.getElementById('nota1').value;
+    var resultado = document.getElementById('resultado');
+    var media = (parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3;
+
+    if (media >= 6){
+        resultado.textContent = "Parabéns! Aprovado com média " + media +".";
+    } else {
+        resultado.textContent = "Reprovado com média " + media + ".";
+    }
+}
