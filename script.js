13d6121591b699ccaa38597753b21f0b7e127e61
José Alves de Oliveira <160446328+professorjose@users.noreply.github.com>
2024-10-29 16:44:35 -0300

    1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function verificar(){
     var n1 = document.getElementById('nota1').value;
-    var n2 = document.getElementById('nota1').value;
-    var n3 = document.getElementById('nota1').value;
+    var n2 = document.getElementById('nota2').value;
+    var n3 = document.getElementById('nota3').value;
     var resultado = document.getElementById('resultado');
     var media = (parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3;
 
